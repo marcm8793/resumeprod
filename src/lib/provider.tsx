ffd9971f989b7481365b05ddfefa3f0368afe456
Marcosss87 <marcmansour@outlook.fr>
2023-09-08 11:49:23 +0200

    amending provider to tsx

--- src/lib/provider.tsx
+++ src/lib/provider.tsx
@@ -1,5 +1,9 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-export default function Provider({ children }) {
+
+interface ProviderProps {
+  children: React.ReactNode;
+}
+export default function Provider({ children }: ProviderProps) {
   return <SessionProvider>{children}</SessionProvider>;
 }
